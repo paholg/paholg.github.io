6ffdde8f069a5eda1021f1debe4459c0bbf11ec8
Paho Lurie-Gregg <paho@paholg.com>
2015-02-18 03:59:49 -0800

    Javascript stuff implemented

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,47 @@
-var offset = '200px';
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var sidebar_height = $('.sidebar').height();
+        var window_height = $(window).height();
 
-// $(window).scroll(function() {
-//     var scroll = $(this).scrollTop();
-//     var height = $('.sidebar').height() + 'px';
+        // sidebar generally fixed, but it should scroll if too big for the screen
+        if (sidebar_height > window_height) {
+            var scroll = $(this).scrollTop();
+            if (sidebar_height - window_height - scroll > 0) {
+                $('.sidebar').css({
+                    'position': 'absolute',
+                    'top': '0',
+                    'bottom': 'auto'
+                });
+            } else {
+                $('.sidebar').css({
+                    'position': 'fixed',
+                    'top': 'auto',
+                    'bottom': '0'
+                });
+            }
+        } else {
+            $('.sidebar').css({
+                'position': 'fixed'
+            });
+        }
+    });
 
-//     $('.sidebar').css({
-//         'position': 'absolute',
-//         'top': '0'
-//     });
-// }
+    // fimxe: in "mobile" view, if you toggle sidebar and then enlarge window, sidebar
+    // doesn't come back
+    $('button').click(function() {
+        $('.sidebar').toggle()
+
+        // this doesn't change anything:
+
+        // if ($('.sidebar').css('display') == 'none') {
+        //     $('.sidebar').css({
+        //         'display': 'block'
+        //     });
+        // } else {
+        //     $('.sidebar').css({
+        //         'display': 'none'
+        //     });
+        // }
+    });
+
+});
