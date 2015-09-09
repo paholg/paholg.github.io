bdffb26348e6e88e61c9eac2d8d52d1ab2997fe6
Paho Lurie-Gregg (via Travis) <paho@paholg.com>
2015-09-09 21:31:04 +0000

    Update doc at add09a5 of dimensioned

--- doc/implementors/core/num/trait.Zero.js
+++ doc/implementors/core/num/trait.Zero.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['dimensioned'] = ["impl&lt;D, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.Zero.html' title='core::num::Zero'>Zero</a> for <a class='struct' href='dimensioned/dimensioned/struct.Dim.html' title='dimensioned::dimensioned::Dim'>Dim</a>&lt;D, V&gt;",];
+implementors['dimensioned'] = ["impl&lt;D, V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/num/trait.Zero.html' title='core::num::Zero'>Zero</a> for <a class='struct' href='dimensioned/dimensioned/struct.Dim.html' title='dimensioned::dimensioned::Dim'>Dim</a>&lt;D, V&gt; <span class='where'>where D: <a class='trait' href='dimensioned/dimensioned/trait.Dimension.html' title='dimensioned::dimensioned::Dimension'>Dimension</a>, V: <a class='trait' href='https://doc.rust-lang.org/nightly/core/num/trait.Zero.html' title='core::num::Zero'>Zero</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/ops/trait.Not.js
+++ doc/implementors/core/ops/trait.Not.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['dimensioned'] = ["impl&lt;D, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='dimensioned/dimensioned/struct.Dim.html' title='dimensioned::dimensioned::Dim'>Dim</a>&lt;D, V&gt;",];
+implementors['dimensioned'] = ["impl&lt;D, V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='dimensioned/dimensioned/struct.Dim.html' title='dimensioned::dimensioned::Dim'>Dim</a>&lt;D, V&gt; <span class='where'>where D: <a class='trait' href='dimensioned/dimensioned/trait.Dimension.html' title='dimensioned::dimensioned::Dimension'>Dimension</a> + <a class='trait' href='dimensioned/peano/trait.Same.html' title='dimensioned::peano::Same'>Same</a>&lt;D&gt;, V: <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a>, D::Output: <a class='trait' href='dimensioned/dimensioned/trait.Dimension.html' title='dimensioned::dimensioned::Dimension'>Dimension</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
